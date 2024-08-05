6776f9cfb097afa1681df4c5b904006b209bcdf3
Faith Kariuki <tcwamboi@gmail.com>
2024-08-05 16:18:25 +0300

    Complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,16 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isSearched, setIsSearched] = useState(false);
+
+  function onSearchChange(event) {
+    const value = event.target.value;
+    const searchedItems = itemData.filter((item) => {
+      return item.name.toLowerCase().includes(value.toLowerCase());
+    });
+    setItems(searchedItems);
+    setIsSearched(true);
+  }
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
@@ -14,7 +24,7 @@ function App() {
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onSearchChange={onSearchChange} />
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,12 +3,14 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onSearchChange }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+
+  
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -19,12 +21,13 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
+
     </div>
   );
 }
